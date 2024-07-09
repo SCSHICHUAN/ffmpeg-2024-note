dab3826c7c8e03c85ae0508148591fb482a8c8b5
SHICHUAN <573387383@qq.com>
2024-07-09 11:04:00 +0800

    修改项目名字为SCPlayer

---
+++ SCPlayer/SCPlayer/SCPlayer.h
@@ -0,0 +1,14 @@
+//
+//  SCPlayer.h
+//  SCFFmpeg
+//
+//  Created by stan on 2024/7/8.
+//  Copyright © 2024 石川. All rights reserved.
+//
+
+#ifndef SCPlayer_h
+#define SCPlayer_h
+
+#include <stdio.h>
+
+#endif /* SCPlayer_h */
---
+++ SCPlayer/SCPlayer/fifo.c
@@ -0,0 +1,9 @@
+//
+//  fifo.c
+//  SCFFmpeg
+//
+//  Created by stan on 2024/7/8.
+//  Copyright © 2024 石川. All rights reserved.
+//
+
+#include "fifo.h"
